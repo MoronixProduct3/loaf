173154afd7131e250bd23feed81b50474cbabe4b
MoronixProduct3 <moronicproduct3@gmail.com>
2017-09-21 00:13:24 -0400

    Debug user limit

--- channel_modules/ChannelManager.js
+++ channel_modules/ChannelManager.js
@@ -293,8 +293,8 @@ class ChannelManager{
             false,
             'Auto-expansion'
         );
-        await nChan.setUserLimit(this.client.channels.get(vectorID).userLimit);
-        nChan.setPosition(lastPosition + 1);
+        await nChan.setPosition(lastPosition + 1);
+        nChan.setUserLimit(this.client.channels.get(vectorID).userLimit);
         expChannels.push(nChan.id);
 
         this.scaledChannels.set(vectorID, expChannels);
